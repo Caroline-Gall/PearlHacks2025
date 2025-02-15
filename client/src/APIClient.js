63efefa79f1b03addb2b42c0e6a9289c0babaaf6
Sebine Jacobson <sebine.jacobson@gmail.com>
2025-02-15 15:42:12 -0500

    add apiclient

---
+++ client/src/APIClient.js
@@ -0,0 +1,28 @@
+import HTTPClient from "./HTTPClient.js";
+const API_BASE = `/api`;
+
+const createUser = (first_name, last_name, email, file_num, file_type, role_id, county_id, company_id, is_admin) => {
+  const data = {
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
+    file_num: file_num,
+    file_type: file_type,
+    role_id: role_id,
+    county_id: county_id,
+    company_id: company_id,
+    is_admin: is_admin
+  }
+  return HTTPClient.post(`${API_BASE}/users`, data);
+};
+
+
+const getAllUsers = () => {
+  return HTTPClient.get(`${API_BASE}/users`);
+};
+
+
+export default {
+  createUser,
+  getAllUsers
+};
